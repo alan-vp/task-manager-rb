80f48981860a9d82c1af1d0c7f699abd5164769e
alan vergara <alan_vergara_p@hotmail.com>
2022-10-04 18:01:44 -0500

    generate routes, Task model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # index
+  get 'tasks', to: 'tasks#index', as: :tasks
+  # new - get
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # create - post
+  post 'tasks', to: 'tasks#create'
+  # show - get
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # edit - get
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # update - patch
+  patch 'tasks/:id', to: 'tasks#update'
+  # destroy - delete
+  delete 'tasks/id', to: 'tasks#destroy'
 end
